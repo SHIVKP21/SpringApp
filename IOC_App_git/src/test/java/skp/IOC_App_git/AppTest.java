b5ffef18c005f78bfab0ecfaccdee766e92893d3
SSD <SSD@DESKTOP-9H664VI>
2025-03-02 18:23:59 +0530

    commit and push

---
+++ IOC_App_git/src/test/java/skp/IOC_App_git/AppTest.java
@@ -0,0 +1,19 @@
+package skp.IOC_App_git;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
